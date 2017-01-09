61a4da0e180108b3b9a6bd506982a14315dcd5bf
Saral Ali Riza Savas <A0217@pad01.aeat.allianz.at>
2017-01-09 14:47:15 +0100

    support Maven without proxy too

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 /usr/local/bin/mvn-entrypoint.sh
-env | grep PROXY_ | /tmp/replace_by_env.sh  /tmp/settings.template.xml | sed '/\$PROXY_/d' | sed '/\$NO_PROXY/d' > /root/.m2/settings.xml
+env | grep -q PROXY_
+if [ $? -eq 1 ]; then
+  echo "Proxy settings not found"
+  sed '/<proxies>/,/<\/proxies>/d' < /tmp/settings.template.xml > /root/.m2/settings.xml
+else
+  echo "Proxy settings found"
+  env | grep PROXY_ | /tmp/replace_by_env.sh  /tmp/settings.template.xml | sed '/\$PROXY_/d' | sed '/\$NO_PROXY/d' > /root/.m2/settings.xml
+fi
 exec "$@"

b5c41b86d4b5e34fb9b4b602102b329e7c88684d
Saral Ali Riza Savas <A0217@pad01.aeat.allianz.at>
2017-01-15 16:05:28 +0100

    use right folders to copy settings

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM maven:3.3.9-jdk-8-alpine
 MAINTAINER Ali Riza Saral aliriza.saral@gmail.com
 
-ADD *.sh /tmp/
-ADD settings.template.xml /tmp/
-ENTRYPOINT ["/tmp/entrypoint.sh"]
+ADD *.sh /usr/local/bin/
+COPY settings.template.xml /usr/share/maven/ref/settings.template.xml
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 env | grep -q PROXY_
 if [ $? -eq 1 ]; then
   echo "Proxy settings not found"
-  sed '/<proxies>/,/<\/proxies>/d' < /tmp/settings.template.xml > /root/.m2/settings.xml
+  sed '/<proxies>/,/<\/proxies>/d' < /usr/share/maven/ref/settings.template.xml > /root/.m2/settings.xml
 else
   echo "Proxy settings found"
-  env | grep PROXY_ | /tmp/replace_by_env.sh  /tmp/settings.template.xml | sed '/\$PROXY_/d' | sed '/\$NO_PROXY/d' > /root/.m2/settings.xml
+  env | grep PROXY_ | /usr/local/bin/replace_by_env.sh  /usr/share/maven/ref/settings.template.xml | sed '/\$PROXY_/d' | sed '/\$NO_PROXY/d' > /root/.m2/settings.xml
 fi
 exec "$@"
